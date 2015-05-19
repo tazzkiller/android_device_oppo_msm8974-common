8def9d713acad5c53bf76e8d3e4a3cac19f060f6
Alexander Martinz <eviscerationls@gmail.com>
2015-05-19 12:13:16 +0200

    msm8974-common: Update firmware symlinks
    Change-Id: I6b8a99f691c65cb780530a21b1a909286701738a
    Signed-off-by: Alexander Martinz <eviscerationls@gmail.com>

--- Android.mk
+++ Android.mk
@@ -4,14 +4,14 @@ ifneq ($(filter find7 find7op,$(TARGET_DEVICE)),)
 # Symlink firmware from /persist
 FIRMWARE_IMAGES := adsp.b00 adsp.b01 adsp.b02 adsp.b03 adsp.b04 adsp.b05 adsp.b06 adsp.b07 adsp.b08 adsp.b09 adsp.b10 adsp.b11 adsp.b12 adsp.mdt \
 	mba.b00 mba.mdt \
-	modem.b00 modem.b01 modem.b02 modem.b03 modem.b04 modem.b05 modem.b08 modem.b10 modem.b11 modem.b13 modem.b14 modem.b15 modem.b16 modem.b17 modem.b18 modem.b19 modem.b20 modem.b21 modem.b22 modem.b25 modem.b26 modem.b27 modem.mdt \
+	modem.b06 modem.b07 modem.b08 modem.b09 modem.b10 modem.b11 modem.b12 modem.b13 modem.b14 modem.b15 modem.b16 modem.b17 modem.b18 modem.b19 modem.b20 modem.b21 modem.b22 modem.b23 modem.b24 modem.b25 modem.b26 modem.b27 modem.mdt \
 	wcnss.b00 wcnss.b01 wcnss.b02 wcnss.b04 wcnss.b06 wcnss.b07 wcnss.b08 wcnss.b09 wcnss.mdt \
 	widevine.b00 widevine.b01 widevine.b02 widevine.b03 widevine.mdt \
 	playread.b00 playread.b01 playread.b02 playread.b03 playread.mdt \
 	mc_v2.b00 mc_v2.b01 mc_v2.b02 mc_v2.b03 mc_v2.mdt \
 	keymaste.b00 keymaste.b01 keymaste.b02 keymaste.b03 keymaste.mdt \
 	isdbtmm.b00 isdbtmm.b01 isdbtmm.b02 isdbtmm.b03 isdbtmm.mdt \
-	cmnlib.b00 cmnlib.b01 cmnlib.b02 cmnlib.mdt \
+	cmnlib.b00 cmnlib.b01 cmnlib.b02 cmnlib.b03 cmnlib.mdt \
 	tqs.b00 tqs.b01 tqs.b02 tqs.b03 tqs.mdt
 
 FIRMWARE_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(notdir $(FIRMWARE_IMAGES)))
